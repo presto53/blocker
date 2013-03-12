7864bb7f47fd54c5ec0d44c9bc318455285ac4b5
Pavel Novitskiy <altusensix@gmail.com>
2013-03-13 00:55:46 +0400

    Fix banhammer class

--- blocker_banhammer.rb
+++ blocker_banhammer.rb
@@ -1,17 +1,22 @@
 class Blocker_banhammer
 
-	def initialize(target)
+	def initialize(target, log)
 		@target = target
+    @log = log
 	end
 
 	def ip_exception?(ip)
 		@target['exceptions'].each do |exception_group|
-			$ip_exceptions[exception_group].each do |exception|
-				if ip == exception
-					return true
-				end
-			end
-		end
+			if $ip_exceptions.key?(exception_group)
+        $ip_exceptions[exception_group].each do |exception|
+           return true if exception.include?(IPAddr.new(ip))
+        end
+      else
+        @log.error "no exception group #{exception_group}."
+        $blocker.exit
+      end
+    end
+    false
 	end
 
 	def ban(ip)

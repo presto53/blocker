f13da54202b1bac977f5c7830c27e5806aca7a89
Pavel Novitskiy <altusensix@gmail.com>
2013-03-13 23:37:34 +0400

    Add Iptables class

---
+++ iptables.rb
@@ -0,0 +1,78 @@
+require 'rubygems'
+require 'posix/spawn'
+
+class Iptables
+
+  attr_reader :thread
+
+  def initialize(ports, chain, protocol)
+    @chain = chain
+    @protocol = protocol
+    @ports = ports.join(',')
+    @results = []
+    @bin = `which iptables`.chomp rescue ''
+
+    raise 'iptables must be run as root' if Process.uid != 0
+    raise 'which or iptables commands not found' if @bin == ''
+
+    @thread = Thread.new do
+      loop do
+        @results.each do |result|
+          raise 'failed to add rule' if result == :unsuccess
+          exit
+        end
+        sleep 5
+      end
+    end
+  end
+
+  def add_rule(source)
+    cmd = "#{@bin} -A #{@chain} -p #{@protocol} -s #{source} -j DROP"
+    run_cmd(cmd)
+  end
+
+  def del_rule(source)
+    cmd = "#{@bin} -D #{@chain} -p #{@protocol} -s #{source} -j DROP"
+    run_cmd(cmd)
+  end
+
+  def list
+
+  end
+
+  def chain_rule
+    cmd = "#{@bin} -L #{@chain}"
+    if not POSIX::Spawn::Child.new(cmd).success?
+      cmd = "#{@bin} -N #{@chain}"
+      raise "cannot add chain #{@chain}" if not POSIX::Spawn::Child.new(cmd).success?
+    end
+    cmd = "#{@bin} -C INPUT -p #{@protocol} -m multiport --dports #{@ports} -j #{@chain}"
+    if not POSIX::Spawn::Child.new(cmd).success?
+      cmd = "#{@bin} -I INPUT 1 -p #{@protocol} -m multiport --dports #{@ports} -j #{@chain}"
+      raise "cannot add rule ***#{cmd}***" if not POSIX::Spawn::Child.new(cmd).success?
+    end
+    true
+  end
+
+  def remove_chain
+    cmd = "#{@bin} -F #{@chain}"
+    raise "cannot flush chain #{@chain}" if not POSIX::Spawn::Child.new(cmd).success?
+    cmd = "#{@bin} -D INPUT -p #{@protocol} -m multiport --dports #{@ports} -j #{@chain}"
+    raise "cannot delete INPUT rule for chain #{@chain}" if not POSIX::Spawn::Child.new(cmd).success?
+    cmd = "#{@bin} -X #{@chain}"
+    raise "cannot delete chain #{@chain}" if not POSIX::Spawn::Child.new(cmd).success?
+  end
+
+  private
+
+  def run_cmd(cmd)
+    Thread.new do
+      if POSIX::Spawn::Child.new(cmd).success?
+        @results << :success
+      else
+        @results << :unsuccess
+      end
+    end
+  end
+
+end

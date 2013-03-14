dac251ecedd955d3cf32eb77cd1372182f6203c2
Pavel Novitskiy <altusensix@gmail.com>
2013-03-14 10:06:04 +0400

    Iptables class

--- iptables.rb
+++ iptables.rb
@@ -27,20 +27,26 @@ class Iptables
   end
 
   def add_rule(source)
-    cmd = "#{@bin} -A #{@chain} -p #{@protocol} -s #{source} -j DROP"
+    cmd = "#{@bin} -A #{@chain} -s #{source} -j DROP"
     run_cmd(cmd)
   end
 
   def del_rule(source)
-    cmd = "#{@bin} -D #{@chain} -p #{@protocol} -s #{source} -j DROP"
+    cmd = "#{@bin} -D #{@chain} -s #{source} -j DROP"
+    run_cmd(cmd)
+  end
+
+  def check_rule(source)
+    cmd = "#{@bin} -C #{@chain} -s #{source} -j DROP"
     run_cmd(cmd)
   end
 
   def list
-
+    cmd = "#{@bin} -L #{@chain} -n"
+    raise "cannot add chain #{@chain}" if not POSIX::Spawn::Child.new(cmd).success?
   end
 
-  def chain_rule
+  def add_chain
     cmd = "#{@bin} -L #{@chain}"
     if not POSIX::Spawn::Child.new(cmd).success?
       cmd = "#{@bin} -N #{@chain}"
